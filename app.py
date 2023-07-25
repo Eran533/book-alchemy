1b744955c22a27bac8cdc76dc413179cab08050c
eran blank <eranblank533@campus.masterschool.co>
2023-07-25 18:15:14 +0300

    add--all

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/library.sqlite'
+db = SQLAlchemy(app)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run()
